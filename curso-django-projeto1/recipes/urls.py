9bd0b1ee774efa0a097ba1e725edecd2fb839e5a
Marlon Alex Andrade <marlon.andrade@aprtech.com.br>
2025-06-03 17:58:43 -0300

    Arquivos estaticos e heranca

--- curso-django-projeto1/recipes/urls.py
+++ curso-django-projeto1/recipes/urls.py
@@ -1,8 +1,12 @@
-from recipes.views import home
-from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
+
+
 
