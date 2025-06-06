58dd56faa7af00cd3a67b11fdcf8762c68048b69
Marlon Alex Andrade <marlon.andrade@aprtech.com.br>
2025-06-06 18:01:56 -0300

    Templates, Models, ORM

--- curso-django-projeto1/recipes/admin.py
+++ curso-django-projeto1/recipes/admin.py
@@ -1,3 +1,15 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Recipe
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+# Segunda forma de fazer
+# admin.site.register(Category, CategoryAdmin)
